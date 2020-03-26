29c51e64086ed8d8f1355af1506afbd8742255eb
hwangyoungwoong <hpky123@naver.com>
2020-03-27 03:37:36 +0900

    폼추가완료

--- project/form_user/password_change.Designer.cs
+++ project/form_user/password_change.Designer.cs
@@ -29,16 +29,24 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(483, 277);
+            this.label1.Location = new System.Drawing.Point(304, 142);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(45, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(372, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 1;
             // 
             // password_change
             // 
@@ -46,6 +54,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1187, 672);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "password_change";
@@ -58,5 +67,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- project/main.cs
+++ project/main.cs
@@ -14,6 +14,7 @@ namespace project
     public partial class main : Form
     {
         // 폼 저장 변수
+        Form[] form_name = new Form[10];
         int form_count = 0;
 
         // 인사기초정보
@@ -30,18 +31,9 @@ namespace project
 
         private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
         {
-            Console.WriteLine(e.Node.Text);
-            // 폼이름을 넣어야함
-            String[] form_name = new String[form_count];
-            for (int i = 0; i < form_name.Length; i++)
-            {
-                Console.WriteLine("form load: " + form_name[i].ToString());
-            }
-
-            insacontrol.Hide();
-            insainfo.Hide();
-            password_chagne.Hide();
-            
+            for (int i=0; i < form_name.Length-1; i++)
+                form_hide(form_name[i]);
+   
             if (e.Node.Text.Equals("인사기초정보"))
             {
                 insainfo.Show();
@@ -63,13 +55,22 @@ namespace project
             main_add(password_chagne);
         }
 
+        // 폼 추가
         public void main_add(Form addform)
         {
             addform.TopLevel = false;
             this.Controls.Add(addform);
             addform.Parent = this.panel1;
             addform.ControlBox = false;
+            form_name[form_count] = addform;
             form_count++;
+            Array.Resize(ref form_name, form_count+1);
+        }
+
+        // 폼 숨기기
+        public void form_hide(Form hideform)
+        {
+            hideform.Hide();
         }
     }
 }
